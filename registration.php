66965b6e08ec78845d9cdc73d0d2cbd2de947083
Md Mostafijur Rahman <mostafijurlive@gmail.com>
2022-07-25 16:06:16 +0600

    Login, Registraion, Forget password
    Login, Registration, Forget password page are ready to go next step

--- registration.php
+++ registration.php
@@ -11,11 +11,11 @@
 	<meta name="robots" content="" />
 	
 	<!-- DESCRIPTION -->
-	<meta name="description" content="EduChamp : Education HTML Template" />
+	<meta name="description" content="PSMS- Students Resitration" />
 	
-	<!-- OG -->
-	<meta property="og:title" content="EduChamp : Education HTML Template" />
-	<meta property="og:description" content="EduChamp : Education HTML Template" />
+	<!-- OG --> 
+	<meta property="og:title" content="PSMS- Students Resitration" />
+	<meta property="og:description" content="PSMS- Students Resitration" />
 	<meta property="og:image" content="" />
 	<meta name="format-detection" content="telephone=no">
 	
@@ -24,7 +24,7 @@
 	<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png" />
 	
 	<!-- PAGE TITLE HERE ============================================= -->
-	<title>EduChamp : Education HTML Template </title>
+	<title>PSMS- Students Resitration</title>
 	
 	<!-- MOBILE SPECIFIC ============================================= -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -53,29 +53,75 @@
 	<div id="loading-icon-bx"></div>
 	<div class="account-form">
 		<div class="account-head" style="background-image:url(assets/images/background/bg2.jpg);">
-			<a href="index.html"><img src="assets/images/logo-white-2.png" alt=""></a>
+			<a href="index.php"><img src="assets/images/logo-white-2.png" alt=""></a>
 		</div>
 		<div class="account-form-inner">
 			<div class="account-container">
 				<div class="heading-bx left">
-					<h2 class="title-head">Sign Up <span>Now</span></h2>
-					<p>Login Your Account <a href="login.html">Click here</a></p>
+					<h2 class="title-head">Student <span>registration</span></h2>
+					<p>Login Your Account <a href="login.php">Click here</a></p>
 				</div>	
 				<form class="contact-bx">
 					<div class="row placeani">
 						<div class="col-lg-12">
 							<div class="form-group">
 								<div class="input-group">
-									<label>Your Name</label>
-									<input name="dzName" type="text" required="" class="form-control">
+									<label>Student Name</label>
+									<input name="st_name" type="text" required="" class="form-control">
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-12">
 							<div class="form-group">
 								<div class="input-group">
-									<label>Your Email Address</label>
-									<input name="dzName" type="email" required="" class="form-control">
+									<label>Email Address</label>
+									<input name="st_email" type="email" required="" class="form-control">
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-12">
+							<div class="form-group">
+								<div class="input-group">
+									<label>Father's Name</label>
+									<input name="st_father" type="email" required="" class="form-control">
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-12">
+							<div class="form-group">
+								<div class="input-group">
+									<label>Fathers's Mobile</label>
+									<input name="st_father_mobile" type="email" required="" class="form-control">
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-12">
+							<div class="form-group">
+								<div class="input-group">
+									<label>Mother's Name</label>
+									<input name="st_mother" type="email" required="" class="form-control">
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-12">
+							<div class="form-group">
+								<label>Gender</label>
+								<br>
+								<label><input type="radio" id="" name="st_gender" value="male" checked> Male</label>
+								<label><input type="radio" id="" name="st_gender" value="female"> Female</label>
+							</div>
+						</div>
+						<div class="col-lg-12">
+							<div class="form-group">
+								<label>Birthday</label>
+								<input name="st_birthday" type="date" class="form-control">
+							</div>
+						</div>
+						<div class="col-lg-12">
+							<div class="form-group">
+							<div class="input-group"> 
+									<label>Address</label>
+									<input name="st_address" type="text" class="form-control" required="">
 								</div>
 							</div>
 						</div>
@@ -83,19 +129,12 @@
 							<div class="form-group">
 								<div class="input-group"> 
 									<label>Your Password</label>
-									<input name="dzEmail" type="password" class="form-control" required="">
+									<input name="st_password" type="password" class="form-control" required="">
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-12 m-b30">
-							<button name="submit" type="submit" value="Submit" class="btn button-md">Sign Up</button>
-						</div>
-						<div class="col-lg-12">
-							<h6>Sign Up with Social media</h6>
-							<div class="d-flex">
-								<a class="btn flex-fill m-r5 facebook" href="#"><i class="fa fa-facebook"></i>Facebook</a>
-								<a class="btn flex-fill m-l5 google-plus" href="#"><i class="fa fa-google-plus"></i>Google Plus</a>
-							</div>
+							<button name="st_submit" type="submit" value="Submit" class="btn button-md st_submit_btn">Sign Up</button>
 						</div>
 					</div>
 				</form>
@@ -118,7 +157,6 @@
 <script src="assets/vendors/owl-carousel/owl.carousel.js"></script>
 <script src="assets/js/main.js"></script>
 <script src="assets/js/contact.js"></script>
-<script src='assets/vendors/switcher/switcher.js'></script>
 </body>
 
 </html>
